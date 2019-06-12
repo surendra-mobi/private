8534a28509f64939bb12ac86e553c668c4a0258f
unknown <kumarsm2405@gmail.com>
2019-06-12 08:59:29 +0530

    bookings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {Routes, RouterModule} from '@angular/router'
+import {HeaderComponent} from './common/header/header.component';
+import { TestComponent } from './test/test.component';
+import { RentalComponent } from './rental/rental.component';
 
+import { RentalModule } from './rental/rental.module';
+import { AuthModule } from './auth/auth.module';
+
+
+const routs:Routes=[{path:'',redirectTo:'rentals', pathMatch:'full'},{path:'test',component:TestComponent}];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TestComponent,
+    
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routs),
+    RentalModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
